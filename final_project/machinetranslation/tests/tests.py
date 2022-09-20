95e4915af97dd1d07f1491bad8db21d26762954c
Wyatt <wyattcannon78@gmail.com>
2022-09-19 17:07:37 -0700

    fixed tests

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -7,6 +7,8 @@ class testEnglishToFrench(unittest.TestCase):
         self.assertEqual(translator.englishToFrench("My friends"), "Mes amis")
     def test_everybody_in_the_club(self):
         self.assertEqual(translator.englishToFrench("Everybody in the club"), "Tout le monde dans le club")
+    def test_doestranslate(self):
+        self.assertNotEqual(translator.englishToFrench("Everybody in the club"), "Everybody in the club") # Assert not equal
     def test_null(self):
         self.assertEqual(translator.englishToFrench(None), "") # Null Case
 class testFrenchToEnglish(unittest.TestCase):
@@ -14,7 +16,9 @@ class testFrenchToEnglish(unittest.TestCase):
         self.assertEqual(translator.frenchToEnglish("Bonjour"), "Hello")
     def test_mes_amis(self):
         self.assertEqual(translator.frenchToEnglish("Mes amis"), "My Friends")
-    def test_tout_le_monde_dans_le_club(self):
+    def test_doestranslate(self):
+        self.assertNotEqual(translator.frenchToEnglish("Tout le monde dans le club"), "Tout le monde dans le club") # ASsert not equal
+    def test_everybody_in_the_club(self):
         self.assertEqual(translator.frenchToEnglish("Tout le monde dans le club"), "Everyone in the club")
     def test_null(self):
         self.assertEqual(translator.frenchToEnglish(None), "") # Null Case
